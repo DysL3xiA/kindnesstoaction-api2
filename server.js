a5c6c5e79ca42c849fd9a4aa575c44d6ba59f04f
Lexi C <37921314+DysL3xiA@users.noreply.github.com>
2021-01-02 12:57:33 -0800

    returning new chime after post

--- server.js
+++ server.js
@@ -63,6 +63,9 @@ app.get("/all_chimes", function(req, res){
  * Optional Arguments: 
  * description
  * image
+ * 
+ * Returns: chime_id, coin_id, coin_num, title, description,
+ * first_name, last_name, email, latitude, longitude 
 *********************************************************/
 
 app.post("/add_chime", function(req, res){ 
@@ -73,7 +76,7 @@ app.post("/add_chime", function(req, res){
     }
     else {
       // add chime and user
-      const chime_id = Promise.resolve(
+      let chime_id = Promise.resolve(
         addRelation([coin_id, req.body.title, req.body.description || 'null',
         req.body.image || 'null'], 'chime')
         );
@@ -88,10 +91,20 @@ app.post("/add_chime", function(req, res){
       // once chime and user are added, add chime_user relation
       Promise.all([chime_id, user_id, address_id])
       .then((values) => {
-        console.log(values);
-        addRelation([values[0], values[1]], 'chime_user');
-        addRelation([values[0], values[2]], 'chime_address');
-      });
+        chime_id = values[0];
+        const chime_user = addRelation([values[0], values[1]], 'chime_user');
+        const chime_address = addRelation([values[0], values[2]], 'chime_address'); 
+        Promise.all([chime_user, chime_address])
+        .then(() => {
+          let new_chime = {
+            text: get_chime,
+            placeholder_arr: [chime_id],
+          };
+          executeParameterQuery(new_chime, function(context){
+            res.send(context);
+          });
+        });
+      })
     }
   });
 });
@@ -258,6 +271,8 @@ ch.id as chime_id,
 cc.id as coin_id,
 cc.coin_num,
 ch.title,
+ch.description,
+ch.image,
 u.first_name,
 u.last_name,
 u.email,
@@ -270,6 +285,32 @@ left join chime_address ca on ca.chime_id = ch.id
 left join addresses aa on aa.id = ca.address_id
 left join chime_user cu on cu.chime_id = ch.id
 left join users u on u.id = cu.user_id`;
+
+/*************************************************
+ * Get single chime
+*************************************************/
+let get_chime = 
+`select 
+ch.id as chime_id,
+cc.id as coin_id,
+cc.coin_num,
+ch.title,
+ch.description,
+ch.image,
+u.first_name,
+u.last_name,
+u.email,
+aa.latitude,
+aa.longitude
+
+from chimes ch
+left join coins cc on ch.coin_id = cc.id
+left join chime_address ca on ca.chime_id = ch.id
+left join addresses aa on aa.id = ca.address_id
+left join chime_user cu on cu.chime_id = ch.id
+left join users u on u.id = cu.user_id
+
+where ch.id = ($1);`;
 
 /*************************************************
  * Insert new coin
